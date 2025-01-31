4ee15b29df40a4f0b4a5022e7f6ae2f8d16164f4
Pallavi <pallavishettigar03@gmail.com>
2025-01-31 21:29:48 +0530

    add changes

---
+++ src/components/navbar.tsx
@@ -0,0 +1,8 @@
+import {Navar as NavbarBs} from "react-bootstrap" 
+export function NavBar (){
+    return <NavBarBs className="bg-white-shadow-sm mb-3">
+        <Container> Nav 
+            
+        </Container>
+        Nav </NavBarBs>
+}
