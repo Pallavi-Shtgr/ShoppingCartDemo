75cd16b04d35a2da2ae979453798d646252e664d
Pallavi <pallavishettigar03@gmail.com>
2025-02-01 20:37:10 +0530

    add changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap'
-import Home from "./pages/home";
-import Store from "./pages/store";
-import About from "./pages/about";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
 import {Navbar} from "./components/Navbar";
+import {NavBar} from "./components/NavBar"
 
 function App() {
 
   return (
-  <Container className="mb-4"> Hi
-     <Routes>
-        <Route path ="/" element={<Home/>}/>
-        <Route path ="/store" element={<Store/>}/>
-        <Route path ="/about" element={<About/>}/>
-    </Routes>
-  </Container>
-  )
+  <>
+   <NavBar />
+    <Container className="mb-4"> Hi
+      <Routes>
+          <Route path ="/" element={<Home/>}/>
+          <Route path ="/store" element={<Store/>}/>
+          <Route path ="/about" element={<About/>}/>
+      </Routes>
+    </Container>
+  </>
+  );
 }
 
 export default App
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,11 @@
-import {Navar as NavbarBs} from "react-bootstrap" 
-export function NavBar (){
-    return <NavBarBs className="bg-white-shadow-sm mb-3">
-        <Container> Nav 
-            
-        </Container>
-        Nav </NavBarBs>
+import { Navbar as NavbarBs, Container } from "react-bootstrap";
+
+export function NavBar() {
+    return (
+        <NavbarBs className="bg-white shadow-sm mb-3">
+            <Container>
+                <NavbarBs.Brand href="#">Nav</NavbarBs.Brand>
+            </Container>
+        </NavbarBs>
+    );
 }
