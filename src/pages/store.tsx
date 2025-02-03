a043360afdf3c5772fb73afcd1a94d94e7e77957
Pallavi <pallavishettigar03@gmail.com>
2025-02-03 22:43:42 +0530

    add changes

--- src/pages/store.tsx
+++ src/pages/store.tsx
@@ -1,17 +1,17 @@
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
 
-import StoreItems from "../data/items.json"
-export default function Store() {
-    return (
+export function Store() {
+  return (
     <> 
-     <h1>Store Page</h1>
-    <Row>
-      {StoreItems.map(item =>(
-          <Col>
-    </Col>
-
-      ) )}
-    
-      </Row> </>
-    )
-  }
-  
+      <h1>Store</h1>
+      <Row>
+        {storeItems.map(item => (
+          <Col key={item.id}>
+            {JSON.stringify(item)}
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+}
