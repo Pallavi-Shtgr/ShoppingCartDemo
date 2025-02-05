bee59d6d637fc90bfe593cc4cae1b239636c26f8
Pallavi <pallavishettigar03@gmail.com>
2025-02-05 21:25:33 +0530

    add changes

--- src/pages/store.tsx
+++ src/pages/store.tsx
@@ -1,5 +1,6 @@
 import { Col, Row } from "react-bootstrap";
 import storeItems from "../data/items.json";
+import { StoreItem } from "../components/StoreItem";
 
 export function Store() {
   return (
@@ -8,7 +9,7 @@ export function Store() {
       <Row md={2} xs={1} lg={3} className="g-3">
         {storeItems.map(item => (
           <Col key={item.id}>
-            {JSON.stringify(item)}
+            <StoreItem {...item} />  
           </Col>
         ))}
       </Row>
