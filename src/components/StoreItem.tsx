c803ccad87bb42f411ed04667cb8cca2355aa8b5
Pallavi <pallavishettigar03@gmail.com>
2025-02-07 23:21:59 +0530

    add changes

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -17,8 +17,9 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
         style={{ objectFit: "cover" }}
       />
       <Card.Body className="d-flex flex-column">
-        <Card.Title className="d-flex justify-content-between align-items-center-baseline">
-          {name}
+        <Card.Title className="d-flex justify-content-between align-items-center-baseline 
+        mb-4">
+           <span className="fs-2">${price}</span>
           <span className="ms-auto">${price}</span>
         </Card.Title>
       </Card.Body>
