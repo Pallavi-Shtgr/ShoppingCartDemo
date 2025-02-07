fe48dd207b0f8be84a031706d77f3d72624ea2b5
Pallavi <pallavishettigar03@gmail.com>
2025-02-07 23:35:20 +0530

    add changes

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap";
+import { formatCurrency } from "../utilities/formatCurrency";
 
 type StoreItemProps = {
   id: number;
@@ -17,10 +18,9 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
         style={{ objectFit: "cover" }}
       />
       <Card.Body className="d-flex flex-column">
-        <Card.Title className="d-flex justify-content-between align-items-center-baseline 
-        mb-4">
-           <span className="fs-2">${price}</span>
-          <span className="ms-2 text-muted">${price}</span>
+        <Card.Title className="d-flex justify-content-between align-items-center mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
       </Card.Body>
     </Card>
